3eff57a5d4dc0a19ecd1f19ad42338e5a2007125
gabylumaldonado <gabylumaldonado@gmail.com>
2023-10-20 18:51:28 -0500

    subirarchivo_y_carpeta

---
+++ Scripts/pseudocode.R
@@ -0,0 +1,14 @@
+#Escribe un codigo que imprima un numero y su cuadrado a lo largo de un rando de valores
+# Tambien que imprima la suma de todos los cuadrados de dicho rango
+
+#Define el valor minimo y el maximo
+
+#Crea una variable squaresum que tenga al inicio cero
+
+#Haz un loop a lo largo de ese rango y por cada valor haz lo siguiente
+	#imprime el valor y el valor al cuadrado
+	#Adiciona el valor al cuadrado a la variable squaresum
+#Una vez terminado el loop imprimime la variable squaresum
+
+
+
